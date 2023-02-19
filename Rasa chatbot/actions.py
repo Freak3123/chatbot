809def3a4992a1a1f8e9bf5e60a0f44bf5ae89e8
Freak3123 <118867504+Freak3123@users.noreply.github.com>
2023-02-20 00:01:10 +0530

    Update actions.py

--- Rasa chatbot/actions.py
+++ Rasa chatbot/actions.py
@@ -15,6 +15,60 @@ from rasa_sdk.executor import CollectingDispatcher
 import sqlite3
 import smtplib
 #
+class ValidateContactUsForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_contact_us_form"
+
+    def validate_name(
+        self,
+        value: Text,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> Dict[str, str]:
+        if value is not None:
+            return {"name": value}
+        else:
+            return {"requested_slot": "name"}
+
+    def validate_email(
+        self,
+        value: Text,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> Dict[str, str]:
+        if value is not None:
+            return {"email": value}
+        else:
+            return {"requested_slot": "email"}
+
+    def validate_phone_number(
+        self,
+        value: Text,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> Dict[str, str]:
+        if value is not None:
+            return {"phone_number": value}
+        else:
+            return {"requested_slot": "phone_number"}
+
+    def validate_confirm_details(
+        self,
+        value: Text,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: "DomainDict",
+    ) -> Dict[str, str]:
+        intent_name = tracker.get_intent_of_latest_message()
+        if value is not None:
+            if intent_name in ["affirm", "deny"]:
+                return {"confirm_details": intent_name}
+        else:
+            return {"requested_slot": "confirm_details"}
+        
 class DisplayFoodMenu(Action):
 
     def name(self) -> Text:
